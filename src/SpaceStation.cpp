73d43608424f8b2a476aab15c67e86e78d487ab9
kko <pioneer@paahdin.com>
2013-05-17 12:51:48 -0400

    Fix a model name issue in stations

--- src/SpaceStation.cpp
+++ src/SpaceStation.cpp
@@ -176,9 +176,9 @@ void SpaceStation::InitStation()
 	for(int i=0; i<NUM_STATIC_SLOTS; i++) m_staticSlot[i] = false;
 	Random rand(m_sbody->seed);
 	bool ground = m_sbody->type == SystemBody::TYPE_STARPORT_ORBITAL ? false : true;
-	if (ground) { 
+	if (ground) {
 		m_type = &SpaceStationType::surfaceStationTypes[ rand.Int32(SpaceStationType::surfaceStationTypes.size()) ];
-	} else { 
+	} else {
 		m_type = &SpaceStationType::orbitalStationTypes[ rand.Int32(SpaceStationType::orbitalStationTypes.size()) ];
 	}
 
@@ -255,10 +255,11 @@ void SpaceStation::UpdateShipyard()
 
 	for (; toAdd > 0; toAdd--) {
 		ShipType::Id id = ships[Pi::rng.Int32(ships.size())];
+		const ShipType& shipType = ShipType::types[id];
 		std::string regId = Ship::MakeRandomLabel();
 		SceneGraph::ModelSkin skin;
 		skin.SetRandomColors(Pi::rng);
-		skin.SetPattern(Pi::rng.Int32(0, Pi::FindModel(id)->GetNumPatterns()));
+		skin.SetPattern(Pi::rng.Int32(0, Pi::FindModel(shipType.modelName)->GetNumPatterns()));
 		skin.SetLabel(regId);
 		ShipOnSale sos(id, regId, skin);
 		m_shipsOnSale.push_back(sos);
@@ -328,7 +329,7 @@ bool SpaceStation::LaunchShip(Ship *ship, int port)
 	const Aabb& aabb = ship->GetAabb();
 	const matrix3x3d mt = ship->GetOrient();
 	const vector3d up = mt.VectorY().Normalized() * aabb.min.y;
-	
+
 	sd.fromPos = (ship->GetPosition() - GetPosition() + up) * GetOrient();	// station space
 	sd.fromRot = Quaterniond::FromMatrix3x3(GetOrient().Transpose() * mt);
 
@@ -419,11 +420,11 @@ bool SpaceStation::OnCollision(Object *b, Uint32 flags, double relVel)
 //   Stage 1 (clearance granted): open
 //           (clearance expired): close
 //   Docked:                      close
-// 
+//
 // Undocking:
 //   Stage -1 (LaunchShip): open
 //   Post-launch:           close
-//   
+//
 
 void SpaceStation::DockingUpdate(const double timeStep)
 {
@@ -493,7 +494,7 @@ void SpaceStation::DockingUpdate(const double timeStep)
 			m_doorAnimationStep = -0.3; // close door
 		}
 	}
-	
+
 	m_doorAnimationState = Clamp(m_doorAnimationState + m_doorAnimationStep*timeStep, 0.0, 1.0);
 	if (m_doorAnimation)
 		m_doorAnimation->SetProgress(m_doorAnimationState);
@@ -522,7 +523,6 @@ void SpaceStation::PositionDockedShip(Ship *ship, int port) const
 		ship->SetOrient(GetOrient() * matrix3x3d::FromVectors(dport.xaxis, dport.yaxis, dport.zaxis));
 	}
 }
-
 
 void SpaceStation::StaticUpdate(const float timeStep)
 {
@@ -597,7 +597,6 @@ bool SpaceStation::IsGroundStation() const
 {
 	return (m_type->dockMethod == SpaceStationType::SURFACE);
 }
-
 
 /* MarketAgent shite */
 void SpaceStation::Bought(Equip::Type t) {
@@ -698,7 +697,6 @@ void SpaceStation::CreateBB()
 	LuaEvent::Queue("onCreateBB", this);
 	m_bbCreated = true;
 }
-
 
 static int next_ref = 0;
 int SpaceStation::AddBBAdvert(std::string description, AdvertFormBuilder builder)
